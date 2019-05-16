d10893259587ebc40d8a54dbf57072ef1a2f41c4
y.silov <y.silov@2ygis.ru>
2019-05-16 18:48:07 +0700

    Есть какой-то калькулятор

--- TestGitBranchesApp/Program.cs
+++ TestGitBranchesApp/Program.cs
@@ -2,6 +2,19 @@
 
 namespace TestGitBranchesApp
 {
+    public static class Calculator
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
